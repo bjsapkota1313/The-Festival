67525592ba409f36fd08a9e64557aa84d882a1fd
MariaRaluca Diaconescu <MRDiaconescu@outlook.com>
2023-02-10 23:07:14 +0100

    Update header.php

--- app/views/header.php
+++ app/views/header.php
@@ -9,45 +9,66 @@
 
 
   </head>
+<style>
 
+  body {margin:0 10% 0 10%;}
+  nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    
+}
+
+ul {
+    list-style: none;
+}
+
+nav ul li {
+    display: inline;
+    padding-right: 20px;
+    text-align: left;
+}
+
+  </style>
 
 <body>
  
-<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4" >
-  <div class="container">
+<nav class="navbar" >
     <a class="navbar-brand" href="/">Visit Haarlem</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item">
-          <a class="nav-link" href="/">Home</a>
+
+      <ul id="leftNav">
+        <li>
+          <a href="/">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">History</a>
+        <li>
+          <a  href="#">History</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Culture</a>
+        <li >
+          <a href="#">Culture</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Food</a>
+        <li >
+          <a  href="#">Food</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">The Festival</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Account
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Manage account</a></li>
-          </ul>
-        </li>
+        <li >
+          <a  href="#">The Festival</a>
+        </li></ul>
+        <ul id="rightNav">
+        <div class="dropdown">
+  <button class="btn btn-light" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+</svg>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Manage account</a></li>
+ 
+  </ul>
+</div>
+
+
 
       </ul>     
-    </div>
-  </div>
+ 
 </nav>
 
 <div class="container">
