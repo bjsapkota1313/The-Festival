b69e4082dcc907ca86ca77a7266ca265160e5a3a
MariaRaluca Diaconescu <MRDiaconescu@outlook.com>
2023-02-10 22:19:07 +0100

    Tabs

--- app/views/header.php
+++ app/views/header.php
@@ -6,10 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visit Haarlem</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+
+
+  </head>
+
+
 <body>
  
-<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4" >
   <div class="container">
     <a class="navbar-brand" href="/">Visit Haarlem</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,6 +36,15 @@
         <li class="nav-item">
           <a class="nav-link" href="#">The Festival</a>
         </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Account
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Manage account</a></li>
+          </ul>
+        </li>
+
       </ul>     
     </div>
   </div>
