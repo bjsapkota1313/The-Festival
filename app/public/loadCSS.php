bb45944437f8caabd255c8bfa4adf3b29df33b75
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-02-15 15:29:27 +0100

    Added styles.css (#8)
    * Added manage account page
    
    * Added styles.css

---
+++ app/public/loadCSS.php
@@ -0,0 +1,41 @@
+<?php function loadCSS() {
+    
+    echo '<style>
+    body {
+        margin: 0 10% 0 10%;
+        background-color: white;
+      }
+    
+      nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    
+      }
+    
+      ul {
+        list-style: none;
+      }
+    
+      nav ul li {
+        display: inline;
+        padding-right: 20px;
+        text-align: left;
+      }
+      label {
+        display: inline-block;
+        width: 100px;
+        text-align: left;
+    }
+
+    img {width: 20%;}
+
+    #updateAccountForm {margin-left:0%;}
+
+      </style>';}
+
+    ?>
+
+
+
+
