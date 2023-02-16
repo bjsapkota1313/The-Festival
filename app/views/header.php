2d4f4dcc6678b4be2aa9ffce6f1b89853646b188
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-02-16 23:09:30 +0100

    Update from main (#10)
    * Added manage account page (#7)
    
    * Update header.php
    
    * Added styles.css (#8)
    
    * Added manage account page
    
    * Added styles.css
    
    * Solve unclear update to repo (#9)
    
    * Added manage account page
    
    * Added styles.css

--- app/views/header.php
+++ app/views/header.php
@@ -1,39 +1,72 @@
+<?php
+
+echo '
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MVC demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+    <title>Festival</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Set viewport for responsiveness -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/22097c36aa.js" crossorigin="anonymous"></script>
+
+    <link href="/css/styles.css" rel="stylesheet" type="text/css">';
+
+//load CSS faster in hosted website
+include 'loadCSS.php';
+loadCSS();
+
+echo
+  '</head>
+
 <body>
+<header>
+  <nav class="navbar">
+    <a class="navbar-brand" href="/">Visit Haarlem</a>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <div class="container">
-        <a class="navbar-brand" href="/">MVC demo</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+    <ul id="leftNav">
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="#">History</a>
+      </li>
+      <li>
+        <a href="#">Culture</a>
+      </li>
+      <li>
+        <a href="#">Food</a>
+      </li>
+      <li>
+        <a href="#">The Festival</a>
+      </li>
+    </ul>
+    <ul id="rightNav">
+      <div class="dropdown">
+        <button class="btn btn-light" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person"
+            viewBox="0 0 16 16">
+            <path
+              d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+          </svg>
         </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/home/article">Articles</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/home/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/home/register">Register</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="/manageAccount">Manage account</a></li>
 
-<div class="container">
+        </ul>
+      </div>
+
+
+
+    </ul>
+
+  </nav>
+  </header>';
+
+
+?>
