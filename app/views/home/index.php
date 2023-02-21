ca227fa2eefaca4e7ff2844fe0de0d5308cf3cf5
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-02-21 16:56:31 +0100

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,6 +5,7 @@ if (!isset($_SESSION)) {
 //error_reporting(E_ALL | E_WARNING);
 
 include("../config/dbconfig.php");
+include __DIR__ . '/../header.php';
 
 
 try {
@@ -13,13 +14,47 @@ try {
   echo "Connection failed: " . $e->getMessage();
 }
 
-?>
-
-<?php
-include __DIR__ . '/../header.php';
 
 echo '<main>
  
+ <img src="./img/imgPlaceholder.png" alt="Home page picture" id="homePageImg">
+
+ <div class="carousel-caption">
+ <div class="d-inline p-2 display-2">PLACEHOLDER</div>
+ <div class="d-inline p-2 display-1">TEXT</div>
+ </div>
+
+ <div class="container object-fit-fill border rounded" id="homePageImagesPanel">
+ <div class="row object-fit-fill border rounded">
+ <div class="col col-sm m-0 p-0">
+ <img src="./img/panelImgPlaceholder.png" alt="" class="homePagePanelImg ">
+ </div>
+ <div class="col-sm m-0 p-0">
+ <img src="./img/panelImgPlaceholder.png" alt="" class="homePagePanelImg">
+ </div>
+ <div class="col-sm m-0 p-0">
+ <img src="./img/panelImgPlaceholder.png" alt="" class="homePagePanelImg">
+ </div>
+ <div class="col-sm m-0 p-0">
+ <img src="./img/panelImgPlaceholder.png" alt="" class="homePagePanelImg">
+ </div>
+</div>
+ </div>
+
+ <div id="homePageVideoPanel">
+
+ <div class="container object-fit-fill border rounded" id="homePageVideo">
+ <div class="row object-fit-fill border rounded">
+ <div class="col col-8 m-0 p-0" >
+ <img src="./img/videoPlaceholder.gif" alt="" class="homePageVideoPlaceholder">
+
+ </div>
+ <div class="col col-4 m-0 p-0" id="homePageLinkToFestival">
+
+ </div>
+ </div>
+ </div>
+ </div>
 
 
 
