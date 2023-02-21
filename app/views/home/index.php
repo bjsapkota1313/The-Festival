9a3333a29896e1a33de72cdccc3c5d799b841329
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-02-21 18:17:40 +0100

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,4 @@
+
 <?php
 if (!isset($_SESSION)) {
   session_start();
@@ -51,12 +52,52 @@ echo '<main>
  </div>
  <div class="col col-4 m-0 p-0" id="homePageLinkToFestival">
 
+ <div class="display-4 my-3 px-2  text-center">PLACEHOLDER TEXT</div>
+
+ <div class="display-5 my-3 px-2  text-center">Placeholder text</div>
+ <div class="display-5 my-3 px-2 text-center">Placeholder text</div>
+
+ <div class="text-center">
+ <button type="button" class="btn btn-outline-secondary my-3 ">Placeholder text</button>
+</div>
+
  </div>
  </div>
  </div>
  </div>
 
 
+
+
+ <div class="container object-fit-fill border rounded" id="homePageMap">
+ <div class="row object-fit-fill border rounded">
+
+ <div class="col col-8 m-0 p-0" >
+ <div class="display-6 my-3"><strong>Placeholder text</strong></div>
+ <img src="./img/mapPlaceholder.png" alt="" class="homePageMapPlaceholder">
+ <div class="display-6 my-3 px-2 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce accumsan eros nisl, non laoreet est aliquam quis. Aliquam pulvinar, ex dignissim fringilla pulvinar, sem nulla vehicula magna, ac lacinia tellus nunc vel ligula. Nulla ut blandit nunc. Morbi suscipit sodales erat et interdum. Nunc vel ipsum augue.</div>
+
+
+ </div>
+
+ <div class="col col-4 m-0 p-0 " >
+ <div class="display-6 my-3 px-4"><strong>Placeholder text</strong></div>
+ <div class="border rounded mx-5 pt-2 pb-5 px-2"  id="homePageInfo" >
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce accumsan eros nisl, non laoreet est aliquam quis. 
+
+ <div  class="mt-5 px-2 ">
+ <p class="d-inline">Placeholder text</p>
+ <img src="./img/iconPlaceholder.png" alt="" class="homePageInfoImg">
+
+ </div>
+
+
+ </div>
+ </div>
+
+
+ </div>
+ </div>
 
  </main>
  </body>
