74322f3c5547618fff2bbbae62852e0ae15bb12e
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-02-21 14:25:21 +0100

    Update loadCSS.php

--- app/public/loadCSS.php
+++ app/public/loadCSS.php
@@ -1,40 +1,31 @@
-<?php function loadCSS() {
-    
-    echo '<style>
+<?php function loadCSS()
+{
+
+  echo '<style>
     body {
-        margin: 0 10% 0 10%;
         background-color: white;
       }
-    
-      nav {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-    
-      }
-    
-      ul {
-        list-style: none;
-      }
-    
-      nav ul li {
-        display: inline;
-        padding-right: 20px;
-        text-align: left;
-      }
+
       label {
         display: inline-block;
         width: 100px;
         text-align: left;
     }
 
-    img {width: 20%;}
+
+    img {width: 40%;}
+
+    #navbarMain { padding-left:5%;
+    padding-right:10%;}
 
     #updateAccountForm {margin-left:0%;}
 
-      </style>';}
+    li:hover {background-color:white;}
 
-    ?>
+      </style>';
+}
+
+?>
 
 
 
