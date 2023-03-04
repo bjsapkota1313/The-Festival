9da7918a981c33962bf536fbf4fe39f83885e135
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2023-03-04 20:18:19 +0100

    Yummy Admin CRUD restaurants (#28)
    Added yummy controller, service, and repository.
    Added CRUD operation for restaurants in the Yummy.
    /festival/Yummy/editRestaurant
    to create new restaurant
    /festival/Yummy/editRestaurant?name=restaurantName
    for view, update, and delete

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -12,9 +12,20 @@ class Controller {
         $view = debug_backtrace()[1]['function'];
         require __DIR__ . "/../views/$directory/$view.php";
     }
+    // since festival files are inside a folder, the path is different.
+    function displayViewFestival($model) {        
+        $directory = strtolower(substr(get_class($this), 0, -10));
+        $view = debug_backtrace()[1]['function'];
+        require __DIR__ . "/../views/festival/$directory/$view.php";
+    }
     function displayPageView($view){
         $directory = strtolower(substr(get_class($this), 0, -10));
         require __DIR__ . "/../views/$directory/$view.php";
+    }
+    // since festival files are inside a folder, the path is different.
+    function displayPageViewFestival($view){
+        $directory = strtolower(substr(get_class($this), 0, -10));
+        require __DIR__ . "/../views/festival/$directory/$view.php";
     }
     protected function parseDateOfBirth($date): bool|string
     {
