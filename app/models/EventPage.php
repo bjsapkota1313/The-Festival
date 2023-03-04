925e6903f84c2bb424af73bdfc2ec45889d3cfd8
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2023-03-04 20:17:43 +0100

    Yummy Admin CRUD restaurants
    Added yummy controller, service, and repository.
    Added CRUD operation for restaurants in the Yummy.
    /festival/Yummy/editRestaurant
    to create new restaurant
    /festival/Yummy/editRestaurant?name=restaurantName
    for view, update, and delete

--- app/models/EventPage.php
+++ app/models/EventPage.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once __DIR__ . '/Content.php';
 
---
+++ app/services/restaurantService.php
@@ -0,0 +1,37 @@
+<?php
+require __DIR__ . '/../repositories/restaurantRepository.php';
+require_once __DIR__ . '/../models/restaurant.php';
+
+class RestaurantService
+{
+    
+    public function deleteRestaurantById(int $restaurantID)
+    {
+        $repository = new RestaurantRepository();
+        return $repository->deleteRestaurantById($restaurantID);
+    }
+    public function getRestaurantByName(string $restaurantName)
+    {
+        $repository = new RestaurantRepository();
+        return $repository->getRestaurantByName($restaurantName);
+    }
+
+
+    public function createNewRestaurant($newRestaurant): void
+    {
+        $repository = new RestaurantRepository();
+        $repository->createNewRestaurant($newRestaurant);
+    }
+
+    public function updateRestaurantById($restaurantID, $newRestaurant): void 
+    {
+        $repository = new RestaurantRepository();
+        $repository->updateRestaurantById($restaurantID, $newRestaurant);
+    }
+
+    public function deletePageById($pageID): void 
+    {
+        $repository = new PageRepository();
+        $repository->deletePageById($pageID);
+    }
+}
