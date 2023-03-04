55051028aefac22c72ba03ad5d5b9595a8e331a6
bjsapkota1313 <90284672+bjsapkota1313@users.noreply.github.com>
2023-03-04 23:15:40 +0100

    Spotify Service

--- app/controllers/festival/DanceController.php
+++ app/controllers/festival/DanceController.php
@@ -1,14 +1,16 @@
 <?php
 require_once __DIR__ . '/EventController.php';
-require_once __DIR__ . '/../../Vendor/autoload.php';
+require_once __DIR__.'/../../services/SpotifyService.php';
+
 
 class DanceController extends eventController
 {
-    private $SpotifyAccessToken;
+    private $SpotifyService;
 
     public function __construct()
     {
         parent::__construct();
+        $this->SpotifyService = new SpotifyService();
     }
 
     public function index()
@@ -23,83 +25,23 @@ class DanceController extends eventController
 
     public function artist()
     {
-        try{
-            $artistAlbums = $this->getArtistAlbums('BTS', 6);
-            $artistTopTracks = $this->getArtistTopTracksWithLimit('BTS', 10,'NL');
-            require __DIR__ . '/../../views/festival/Dance/artist.php';
-        }
-        catch (\SpotifyWebAPI\SpotifyWebAPIAuthException $e){
-            echo $e->getMessage();
-        }
-
-    }
-
-    private function createSpotifySession()
-    {
         try {
-            require __DIR__ . '/../../config/RESTAPIsConfig/SpotifyConfig.php';
-            $session = new SpotifyWebAPI\Session(
-                $clientId,
-                $clientSecret
-            );
-            $session->requestCredentialsToken();
-            $this->SpotifyAccessToken = $session->getAccessToken();
-            $api = new SpotifyWebAPI\SpotifyWebAPI();
-            $api->setAccessToken($this->SpotifyAccessToken);
-            return $api;
-        } catch ( \SpotifyWebAPI\SpotifyWebAPIException | \SpotifyWebAPI\SpotifyWebAPIAuthException  $e) {
+            $artistAlbums = $this->SpotifyService->getArtistAlbumsWithLimit('Prakash saput', 6);
+            $artistTopTracks = $this->SpotifyService->getArtistTopTracksWithLimit('Prakash saput', 10);
+            require __DIR__ . '/../../views/festival/Dance/artist.php';
+        } catch (\SpotifyWebAPI\SpotifyWebAPIAuthException $e) {
             echo $e->getMessage();
         }
 
     }
+
+
 
     public function test()
     {
-        try {
-            $api = $this->createSpotifySession();
-            if (!empty($api)) {
-                $artistName = 'Martin Garrix';
-                // Search for the artist
-                $results = $api->search($artistName, 'artist');
-                // Get the first artist from the results
-                $artist = $results->artists->items[0];
-                // Get the artist's albums
-                $albums = $api->getArtistAlbums($artist->id, [
-                    'limit' => 5
-                ]);
-                // Do something with the track details...
-                // Loop through the albums and print their names
-                foreach ($albums->items as $album) {
-//                    echo substr($album->name,0,7) ."....". "<br>";
-                    echo $this->getFormattedStringToDisplay($album->name, 10) . "<br>";
-                }
-            } else {
-                echo "No access token";
-            }
-        } catch (\SpotifyWebAPI\SpotifyWebAPIException | \SpotifyWebAPI\SpotifyWebAPIAuthException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    /**
-     * @throws \SpotifyWebAPI\SpotifyWebAPIAuthException
-     */
-    private function getArtistAlbums($artistName, $limit): object|array
-    {
-        $api = $this->createSpotifySession();
-        if (!empty($api)) {
-            // Search for the artist
-            $results = $api->search($artistName, 'artist');
-            // Get the first artist from the results
-            $artist = $results->artists->items[0];
-            // Get the artist's albums
-            $albums = $api->getArtistAlbums($artist->id, [
-                'limit' => $limit
-            ]);
-            return $albums;
-        } else {
-            throw  new \SpotifyWebAPI\SpotifyWebAPIAuthException("Unable to connect with spotify");
-        }
+       $ip= $_SERVER['REMOTE_ADDR'];
+       $countrycode = geoip_country_code_by_name($ip);
+        echo $countrycode;
     }
 
     private function getFormattedStringToDisplay($string, $length): string
@@ -111,28 +53,6 @@ class DanceController extends eventController
         }
     }
 
-    /**
-     * @throws \SpotifyWebAPI\SpotifyWebAPIAuthException
-     */
-    private function getArtistTopTracksWithLimit($artistName, $limit, $marketCountry): object|array
-    {
-        $api = $this->createSpotifySession();
-        if (!empty($api)) {
-            // Search for the artist
-            $results = $api->search($artistName, 'artist');
-            // Get the first artist from the results
-            $artist = $results->artists->items[0];
-            // Get the artist's top tracks in the selecected country
-            $topTracks = $api->getArtistTopTracks($artist->id, [
-                'market' => $marketCountry
-            ]);
 
-            // returning desired track limit
-            return  array_slice($topTracks->tracks, 0, $limit);
-
-        } else {
-            throw  new \SpotifyWebAPI\SpotifyWebAPIAuthException("Unable to connect with spotify");
-        }
-    }
 
 }
---
+++ app/services/SpotifyService.php
@@ -0,0 +1,91 @@
+<?php
+require_once __DIR__ . '/../Vendor/autoload.php';
+class SpotifyService
+{
+    private $spotifyAPI;
+    private $cilentCountryCode; // required to search the artist top tracks and albums so that we can get the results according to result country client country
+
+    public function __construct()
+    {
+        $this->spotifyAPI = $this->createSpotifySession();
+        $this->cilentCountryCode = 'NL';
+//        if(empty($this->cilentCountryCode)){  //ToDO :  for check Need to ask
+//            $this->cilentCountryCode = 'NL';
+//        }
+    }
+    private function createSpotifySession()
+    {
+        require_once __DIR__ . '/../config/RESTAPIsConfig/SpotifyConfig.php'; // getting the config file
+        try {
+            $session = new SpotifyWebAPI\Session(
+                $clientId,
+                $clientSecret
+            );
+            $session->requestCredentialsToken();
+           $accessToken= $session->getAccessToken();
+            $api = new SpotifyWebAPI\SpotifyWebAPI();
+            $api->setAccessToken($accessToken);
+            return $api;
+        } catch (\SpotifyWebAPI\SpotifyWebAPIException | \SpotifyWebAPI\SpotifyWebAPIAuthException  $e) {
+            echo $e->getMessage();
+        }
+
+    }
+
+    /**
+     * @throws \SpotifyWebAPI\SpotifyWebAPIAuthException
+     */
+    private function getArtistIDByArtistName($artistName){
+        if(empty($this->spotifyAPI)){
+            throw  new \SpotifyWebAPI\SpotifyWebAPIAuthException("Unable to connect with spotify");
+        }
+        // Get the first artist from the results
+        $results = $this->spotifyAPI->search($artistName, 'artist');
+        // Get the first artist from the results
+        $artist = $results->artists->items[0];
+
+        return $artist->id;
+    }
+
+    /**
+     * @throws \SpotifyWebAPI\SpotifyWebAPIAuthException
+     */
+    public  function getArtistTopTracksWithLimit($artistName, $limit): object|array
+    {
+        if (!empty($this->spotifyAPI)) {
+
+            $topTracks = $this->spotifyAPI->getArtistTopTracks($this->getArtistIDByArtistName($artistName), [
+                'market' => $this->cilentCountryCode
+            ]);
+
+            // returning desired track limit
+            return array_slice($topTracks->tracks, 0, $limit);
+
+        } else {
+            throw  new \SpotifyWebAPI\SpotifyWebAPIAuthException("Unable to connect with spotify");
+        }
+    }
+
+    /**
+     * @throws \SpotifyWebAPI\SpotifyWebAPIAuthException
+     */
+    public function getArtistAlbumsWithLimit($artistName, $limit): object|array
+    {
+        if (!empty($this->spotifyAPI)) {
+            // Get the artist's albums
+            return $this->spotifyAPI->getArtistAlbums($this->getArtistIDByArtistName($artistName), [
+                'limit' => $limit,
+                'market' => $this->cilentCountryCode
+
+            ]);
+        } else {
+            throw  new \SpotifyWebAPI\SpotifyWebAPIAuthException("Unable to connect with spotify");
+        }
+    }
+    private function getClientCountryCode() :string{
+        // Get client's IP address
+        $clientIP = $_SERVER['REMOTE_ADDR'];
+        // Get client's country code
+        return file_get_contents("https://ipapi.co/{$clientIP}/country_code");
+    }
+}
