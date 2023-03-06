7039baed8bb4a969b7dbe781659a31453d10a0a1
bjsapkota1313 <90284672+bjsapkota1313@users.noreply.github.com>
2023-03-06 11:35:38 +0100

    B ijay's branch (#30)
    * Before Last one
    
    * Git Ignore
    
    * Workk
    
    * Register NEw User
    
    * Edit user
    
    * Sprint -1 completed expect REST Convention and will be done after learning  API Security
    
    -> Online database connection
    ->config File
    ->Sprint 1
    
    * Event Controllers<Music Contoller and FEstival Section
    
    * The festival section
    
    * Dance Thingssssss
    
    * Push First
    
    * BEfore PSotify APIII
    
    * SPotify With WOrking
    
    * Spotify Service
    
    * Country code by IP
    
    * update

--- app/controllers/festival/DanceController.php
+++ app/controllers/festival/DanceController.php
@@ -1,21 +1,44 @@
 <?php
 require_once __DIR__ . '/EventController.php';
+require_once __DIR__ . '/../../services/SpotifyService.php';
+class DanceController extends eventController
+{
+    private $spotifyService;
 
-class DanceController extends eventController {
     public function __construct()
     {
         parent::__construct();
+        $this->spotifyService = new SpotifyService();
     }
-    public function index(){
+
+    public function index()
+    {
         $dancePage = $this->eventPageService->getEventPageByName('Dance/Intro');
-        $bodyHead= $dancePage->getContent()->getBodyHead();
-        $sectionText = $dancePage->getContent()->getSectionText();
+        $bodyHead = $dancePage->getContent()->getBodyHead();
+         $sectionText = $dancePage->getContent()->getSectionText();
         $paragraphs = $sectionText->getParagraphs();
         require __DIR__ . '/../../views/festival/Dance/index.php';
+      }
+
+
+    public function artist()
+    {
+        try {
+            $artistAlbums = $this->spotifyService->getArtistAlbumsWithLimit('Prakash saput', 6);
+            $artistTopTracks = $this->spotifyService ->getArtistTopTracksWithLimit('Prakash saput', 10);
+            require __DIR__ . '/../../views/festival/Dance/artist.php';
+        } catch (\SpotifyWebAPI\SpotifyWebAPIAuthException $e) {
+            echo $e->getMessage();
+        }
 
     }
-    public function artist(){
-       require __DIR__ . '/../../views/festival/Dance/artist.html';
+    private function getFormattedStringToDisplay($string, $length): string
+    {
+        if (strlen($string) > $length) {
+            return substr($string, 0, $length) . "....";
+        } else {
+            return $string;
+        }
     }
 
 }
