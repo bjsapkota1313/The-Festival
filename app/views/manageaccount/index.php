2a6eb789ea254b9b6e4a6946e93951fd6d98e8af
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-06 13:24:23 +0100

    Update index.php

--- app/views/manageaccount/index.php
+++ app/views/manageaccount/index.php
@@ -1,24 +1,23 @@
-<?php
-if (!isset($_SESSION)) {
-    session_start();
-}
-//error_reporting(E_ALL | E_WARNING);
+<?php //load CSS faster in hosted website
+include 'loadCSS.php';
 
-include __DIR__ . '/../header.php';
+//load module for displaying view
+include 'displayViewModule.php'; ?>
 
 
-echo '<main
+
+<main>
 
 <div class="container d-flex justify-content-center align-items-center pt-5">
 <div class="row justify-content-center">
     <div class="col-lg-8 col-md-10">
         <div class="card shadow-sm">
             <div class="card-body">
-                <h1 class="card-title text-center mb-4">' . $currentUser->getFirstName() . ' ' . $currentUser->getLastName() . '</h2><br>
+                <h1 class="card-title text-center mb-4"><?= $currentUser->getFirstName() . ' ' . $currentUser->getLastName() ?></h2><br>
                 <form method="POST" enctype="multipart/form-data" id="updateForm" >
                     <div class="row mb-3 justify-content-center">
                             <div class="profile-img-container mb-3 text-center" >
-                              <img src="' . $currentUser->getPicture() . '"alt="Profile Picture" class="img-fluid">
+                              <img src="<?= $currentUser->getPicture() ?>"alt="Profile Picture" class="img-fluid">
                               <div class="mb-3">
                               <br><br>
                               <label>Change your picture:</label>
@@ -30,26 +29,25 @@ echo '<main
                             <div class="row mb-3 ">
                                 <div class="col-md-6">
                                         <label for="first-name" class="form-label">First Name</label>
-                                        <input type="text" id="firstname" class="form-control" name="firstName" value="' . $currentUser->getFirstName() . '" required>
+                                        <input type="text" id="firstname" class="form-control" name="firstName" value="<?= $currentUser->getFirstName() ?>" required>
                                     </div>
                                 
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label for="last-name" class="form-label">Last Name</label>
-                                        <input type="text" class="form-control" name="lastName" value="' . $currentUser->getLastName() . '" required>
+                                        <input type="text" class="form-control" name="lastName" value="<?= $currentUser->getLastName() ?>" required>
                                     </div>
                                 </div>
-                            </div>';
+                            </div>
 
 
-                            echo ' <div class="row mb-3" id="userRole">
-                           <div class="col-md-6">';
-                            DisplayPage($currentUser);
+                           <div class="row mb-3" id="userRole">
+                           <div class="col-md-6">
+                            <?php DisplayManageAccountPage($currentUser); ?>
 
-                            echo
-                                '<div class="mb-3">
+                             <div class="mb-3">
                                     <select name="userRole" >
-                                    <option value="none" selected disabled hidden>' . Roles::getLabel($currentUser->getRole()) . '</option>
+                                    <option value="none" selected disabled hidden><?= Roles::getLabel($currentUser->getRole()) ?></option>
                                     <option value="Customer">Customer</option>
                                     <option value="Administrator">Administrator</option>
                                     <option value="Employee">Employee</option>
@@ -61,7 +59,7 @@ echo '<main
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label for="email" class="form-label">Email</label>
-                                        <input type="email" class="form-control" name="email" value="' . $currentUser->getEmail() . '" required>
+                                        <input type="email" class="form-control" name="email" value="<?= $currentUser->getEmail() ?>" required>
                                     </div>
                                 </div>
                             </div>
@@ -69,7 +67,7 @@ echo '<main
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label for="dob" class="form-label">Date of Birth</label>
-                                        <input type="date" name="dateOfBirth" class="form-control" value="' . $currentUser->getDateOfBirth()->format('Y-m-d') . '"required>
+                                        <input type="date" name="dateOfBirth" class="form-control" value="<?= $currentUser->getDateOfBirth()->format('Y-m-d') ?>"required>
                                     </div>
                                 </div>
                             </div>
@@ -98,21 +96,20 @@ echo '<main
                                 <label for="password" class="form-label">Confirm password</label>
                                 <input type="password" name="confirmPassword" id="confirmPassword" class="form-control" value="">    
                             </div>
-                            </div>';
-                            echo '</div>
-                            <p id="validationMessage"></p>';
-                            showPasswordFields();
-                            echo '</div>
+                            </div>
+                            </div>
+                            <p id="validationMessage"></p>
+                            <?php showPasswordFields();?>
+                            </div>
                             <div class="row my-3">
                                 <div class="col-md-6">
                                     <div class="d-grid gap-2">
                                         <button type="submit" id="submit" name="updateProfile" class="btn btn-primary btn-lg">Save Changes</button>
                                     </div>
-                                </div>';
-                            submitAccountInfo();
-                            enableSubmitButton();
-                            echo
-                                '<div class="col-md-6">
+                                </div>
+                            <?php submitAccountInfo(); ?>
+                             <?php enableSubmitButton(); ?>
+                            <div class="col-md-6">
                                     <div class="d-grid gap-2">
                                         <button type="reset" class="btn btn-secondary btn-lg" onclick="history.back()">Cancel</button>
                                     </div>
@@ -125,14 +122,10 @@ echo '<main
         </div>
     </div>
 </div>
-</div>';
-echo '
+</div>
 
 
 </main>
 </body>
 </html>
-';
 
-include __DIR__ . '/../footer.php';
-?>
