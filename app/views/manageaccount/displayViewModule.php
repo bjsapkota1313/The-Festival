52225edbbc856367249c1adfc278705680991a0b
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-06 16:04:02 +0100

    Update displayViewModule.php

--- app/views/manageaccount/displayViewModule.php
+++ app/views/manageaccount/displayViewModule.php
@@ -1,12 +1,5 @@
 <?
 
-function DisplayPage($currentUser)
-{
-    if ($currentUser->getRole() == Roles::Customer()) {
-        echo '<script >document.getElementById("manageUsersLink").style.display = "none";</script>';
-    }
-}
-
 
 function DisplayManageAccountPage($currentUser)
 {
@@ -16,57 +9,5 @@ function DisplayManageAccountPage($currentUser)
     }
 }
 
-
-function showPasswordFields()
-{
-
-    echo "<script > 
-    $('#changePasswordCheckBox').click(function(){
-    $('#password-fields').slideToggle();
-    });</script>";
-
-}
-
-
-function submitAccountInfo()
-{
-
-    echo '<script > 
-    $(document).ready(function(){   
-
-
-        $("#updateForm").on("submit",function(event){ 
-            if ($("#changePasswordCheckBox").prop("checked"))  {
-    var newPassword = $("#newPassword").val();
-    var confirmPassword = $("#confirmPassword").val();
-
-    if(newPassword !== confirmPassword){
-    $("#validationMessage").text("Passwords Do Not Match!");
-        $("#submit").prop("disabled", true);
-        event.preventDefault();
-    
-    }    
-    else {
-        event.currentTarget.submit();   
-    }
-}
-    });
-});</script>';
-}
-
-function enableSubmitButton()
-{
-
-    echo '<script > $("#newPassword").on("input", function(e){
-        $("#submit").prop("disabled", false);
-       });
-       $("#confirmPassword").on("input", function(e){
-        $("#submit").prop("disabled", false);
-       });
-       
-       
-       </script>';
-
-}
 
 ?>
