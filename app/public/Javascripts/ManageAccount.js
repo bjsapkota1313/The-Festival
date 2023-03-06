27829283a95b73d5ec6aecb0a91c63c87f471cac
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-06 16:05:47 +0100

    Add files via upload

---
+++ app/public/Javascripts/ManageAccount.js
@@ -0,0 +1,55 @@
+
+
+function showPasswordFields() {
+
+
+    $('#changePasswordCheckBox').click(function () {
+        $('#password-fields').slideToggle();
+    });
+    alert("test");
+
+}
+
+
+function submitAccountInfo() {
+
+
+    $(document).ready(function () {
+
+
+        $("#updateForm").on("submit", function (event) {
+            if ($("#changePasswordCheckBox").prop("checked")) {
+                var newPassword = $("#newPassword").val();
+                var confirmPassword = $("#confirmPassword").val();
+
+                if (newPassword !== confirmPassword) {
+                    $("#validationMessage").text("Passwords Do Not Match!");
+                    $("#submit").prop("disabled", true);
+                    event.preventDefault();
+
+                }
+                else {
+                    event.currentTarget.submit();
+                }
+            }
+        });
+    });
+}
+
+function enableSubmitButton() {
+
+    $("#newPassword").on("input", function (e) {
+        $("#submit").prop("disabled", false);
+    });
+    $("#confirmPassword").on("input", function (e) {
+        $("#submit").prop("disabled", false);
+    });
+
+}
+
+
+showPasswordFields();
+submitAccountInfo();
+enableSubmitButton();
+
+
