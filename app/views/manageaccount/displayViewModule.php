b21ccf6c2ba22f4c7fe7fe11102fbea90bd6988d
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-06 16:37:36 +0100

    Update displayViewModule.php

--- app/views/manageaccount/displayViewModule.php
+++ app/views/manageaccount/displayViewModule.php
@@ -1,8 +1,10 @@
-<?
+<?php
+
 function DisplayManageAccountPage($currentUser)
 {
     if ($currentUser->getRole() == Roles::Customer()) {
         echo '<script >document.getElementById("userRole").style.display = "none";</script>';
     }
 }
+
 ?>
