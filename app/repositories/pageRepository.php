e09b18f65b34730b5a846e47c32c722ab2286b3c
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-08 02:13:06 +0100

    Update pageRepository.php

--- app/repositories/pageRepository.php
+++ app/repositories/pageRepository.php
@@ -117,5 +117,38 @@ class PageRepository extends Repository
             echo $e;
         }
     }
+    
+    
+        private function checkPageExistence($stmt): bool
+    {
+        try {
+            $stmt->execute();
+            if ($stmt->rowCount() > 0) {
+                return true;
+            }
+            return false;
+        } catch (PDOException $e) {
+            $message = '[' . date("F j, Y, g:i a e O") . ']' . $e->getMessage() . $e->getCode() . $e->getFile() . ' Line ' . $e->getLine() . PHP_EOL;
+            error_log("Database connection failed: " . $message, 3, __DIR__ . "/../Errors/error.log");
+            http_response_code(500);
+            exit();
+        }
+    }
+
+    public function  getPageByIdWithUrl($id)
+    {
+        try {
+            
+            $stmt = $this->connection->prepare("SELECT * From Page WHERE id LIKE :id");
+            $stmt->bindValue(':id', "%$id%");
+             if ($this->checkPageExistence($stmt)) {
+                $stmt->execute();
+                $result = $stmt->fetch();
+                return $result;}
+            
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
 }
 
