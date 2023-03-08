900c694fd18bd7c06b459c8cd9c214094435ba3a
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-08 01:58:06 +0100

    Add files via upload

---
+++ app/API/Controllers/PagesController.php
@@ -0,0 +1,35 @@
+<?php
+require __DIR__ . '/../../Services/pageService.php';
+require __DIR__ . '/controller.php';
+
+//TODO: Make REST API Principles
+class PagesController extends Controller
+{
+    private $pageService;
+
+    public function __construct()
+    {
+        $this->pageService = new PageService();
+    }
+
+    public function retrievePageData(){
+        try {
+            $this->sendHeaders();
+            $page=NULL;
+
+            if (!empty($_GET['id'])) {
+                $id = htmlspecialchars($_GET['id']);
+                $page = $this->pageService->getPageByIdWithUrl($id);
+            }
+            echo JSon_encode($page);
+        }
+        catch (InvalidArgumentException|Exception $e) {
+            http_response_code(500); // sending bad request error to APi request if something goes wrong
+            echo $e->getMessage();
+        }
+
+    }
+}
+
+
+?>
