d3356f61bb0fe5198f406903e1f8aebe5aa5d3f4
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-11 16:10:11 +0100

    Update show.php

--- app/views/page/show.php
+++ app/views/page/show.php
@@ -11,7 +11,9 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
   <script src="/Javascripts/tinymce/js/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
-  <link href="/../css/styles.css" rel="stylesheet" type="text/css">
+  <link href="/../css/homePageStyle.css" rel="stylesheet" type="text/css">
+
+
 </head>
 
 <body>
