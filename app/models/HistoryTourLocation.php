704d3c57230611e7dbdb9d945ba5a13e40c6936b
hohyunLee96 <90310107+hohyunLee96@users.noreply.github.com>
2023-03-13 14:06:28 +0100

    David (#34)
    * split registerUser into small functions
    
    * cleaned my code
    
    * online database
    
    * design ui for history
    
    * gps api
    
    * trying to make timetable
    
    * dynamic pages

--- app/models/HistoryTourLocation.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/Location.php';
-
-class HistoryTourLocation extends Location
-{
-
-}
--- app/services/HistoryService.php
+++ app/services/HistoryService.php
@@ -4,15 +4,27 @@ require_once __DIR__ . '/../repositories/HistoryPageRepository.php';
 class HistoryService
 {
     private $repository;
+    private $repository2;
 
     public function __construct()
     {
         $this->repository = new HistoryPageRepository();
+        $this->repository2 = new HistoryEventRepository();
+
     }
     public function getGoogleMarkerByLocationName(string $locationName,$locationPostCode){
         return $this->repository->getGoogleMarkerByLocationName($locationName,$locationPostCode);
     }
     public function getAllHistoryTourLocation(){
         return $this->repository->getAllHistoryTourLocation();
+    }
+    public function getHistoryTourTimeTable(){
+        return $this->repository->getHistoryTourTimeTable();
+    }
+    public function getHistoryTourLocationsByHistoryTourId($historyTourId){
+        return $this->repository2->getHistoryTourLocationsByHistoryTourId($historyTourId);
+    }
+    public function getHistoryTourImageByHistoryTourId($historyTourLocationId){
+        return $this->repository2->getHistoryTourImageByHistoryTourId($historyTourLocationId);
     }
 }
