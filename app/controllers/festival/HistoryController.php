ec68095feda83fdbb32c9826ae741255210d79b3
hohyunLee <hhhhhhlee@naver.com>
2023-03-15 14:26:44 +0100

    I did a lot of things

--- app/controllers/festival/HistoryController.php
+++ app/controllers/festival/HistoryController.php
@@ -34,8 +34,10 @@ class HistoryController extends EventController
         $locationId = $_GET["locationId"];
         $location = $_GET["location"];
         $locationPostCode = $_GET["locationPostCode"];
-          $getLocationParagraphsById = $this->historyService->getHistoryTourLocationsByHistoryTourId($location);
-        $tourImage = $getLocationParagraphsById->getTourImage();
+//        $getLocationParagraphsById = $this->historyService->getHistoryTourLocationsByLocationName(3);
+        $historyTourLocationObject = $this->historyService->getHistoryTourLocationByLocationId($locationId);
+
+//        $tourImage = $getLocationParagraphsById->getTourImage();
 
 
         $this->displayNavBar("A stroll Through History", '/css/festival/history.css');
@@ -60,8 +62,8 @@ class HistoryController extends EventController
         // Group the tours by date and time
         $groupedHistoryTours = array();
         foreach ($historyTours as $historyTour) {
-            $date = $historyTour->getDate()->format('Y-m-d');
-            $time = $historyTour->getDate()->format('H:i');
+            $date = $historyTour->getTourDate()->format('Y-m-d');
+            $time = $historyTour->getTourDate()->format('H:i');
             if (!isset($groupedHistoryTours[$date])) {
                 $groupedHistoryTours[$date] = array();
             }
