a8447c549ebe4ba8e40ed71606917ec0837f3c19
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-17 14:27:19 +0100

    Update EventAccessController.php

--- app/controllers/festival/EventAccessController.php
+++ app/controllers/festival/EventAccessController.php
@@ -1,15 +1,21 @@
 <?php
 require_once __DIR__ . '/EventController.php';
 require_once __DIR__. '/../../services/AvailableEventService.php';
+require_once __DIR__. '/../../services/EventDateService.php';
+
 
 class EventAccessController extends eventController
 {
     private $availableEventService;
+    private $eventDateService;
+
 
     public function __construct()
     {
         parent::__construct();
         $this->availableEventService = new AvailableEventService();
+        $this->eventDateService = new EventDateService();
+
     }
 
     public function index()
@@ -20,11 +26,36 @@ class EventAccessController extends eventController
         $bodyHead =  $eventAccesPage->getContent()->getBodyHead();
         $sectionText =  $eventAccesPage->getContent()->getSectionText();
         $paragraphs = $sectionText->getParagraphs();
-        $availableEvents = $this->availableEventService->getAvailableEventsByEventType('type');
-        $availableEventsList1 = $this->availableEventService->getAvailableEventsByDate($availableEvents, 1);
-        $availableEventsList2 = $this->availableEventService->getAvailableEventsByDate($availableEvents, 2);
-        $availableEventsList3 = $this->availableEventService->getAvailableEventsByDate($availableEvents, 3);
-        $availableEventsList4 = $this->availableEventService->getAvailableEventsByDate($availableEvents, 4);
+        $availableHistoryEvents = $this->availableEventService->getAvailableHistoryEvents();
+        //$availableConcertEvents = $this->availableEventService->getAvailableMusicEvents();
+        $availableMusicEvents = $this->availableEventService->getAvailableMusicEventsData();
+
+        $firstEventsDay = $this->eventDateService->getEventDateById(1);
+        $secondEventsDay = $this->eventDateService->getEventDateById(2);
+        $thirdEventsDay = $this->eventDateService->getEventDateById(3);
+        $fourthEventsDay = $this->eventDateService->getEventDateById(4);
+
+
+        $availableHistoryEventsList1 = $this->availableEventService->getAvailableHistoryEventsByDate($availableHistoryEvents, 1);
+        $availableConcertEventsList1 = $this->availableEventService->getAvailableMusicEventsByDate($availableMusicEvents, 1);
+        $availableEventsList1 = array_merge($availableHistoryEventsList1,  $availableConcertEventsList1);
+
+        $availableHistoryEventsList2 = $this->availableEventService->getAvailableHistoryEventsByDate($availableHistoryEvents, 2);
+        $availableConcertEventsList2 = $this->availableEventService->getAvailableMusicEventsByDate($availableMusicEvents, 2);
+        $availableEventsList2 = array_merge($availableHistoryEventsList2,  $availableConcertEventsList2);
+
+
+        $availableHistoryEventsList3 = $this->availableEventService->getAvailableHistoryEventsByDate($availableHistoryEvents, 3);
+        $availableConcertEventsList3 = $this->availableEventService->getAvailableMusicEventsByDate($availableMusicEvents, 3);
+        $availableEventsList3 = array_merge($availableHistoryEventsList3,  $availableConcertEventsList3);
+
+
+        $availableHistoryEventsList4 = $this->availableEventService->getAvailableHistoryEventsByDate($availableHistoryEvents, 4);
+        $availableConcertEventsList4 = $this->availableEventService->getAvailableMusicEventsByDate($availableMusicEvents, 4);
+        $availableEventsList4 = array_merge($availableHistoryEventsList4,  $availableConcertEventsList4);
+
+
+       
 
         require __DIR__ . '/../../views/festival/EventAccess/eventsPage.php';
         
@@ -32,3 +63,4 @@ class EventAccessController extends eventController
 
     }}
 ?>
+
