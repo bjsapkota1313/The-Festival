ccc918801b28cfc35b11195dda1b5071e4a1822d
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-17 14:37:32 +0100

    Update ArtistService.php

--- app/services/ArtistService.php
+++ app/services/ArtistService.php
@@ -28,5 +28,9 @@ class ArtistService
     public function getAllParticipatingArtistsInPerformance($artistPerformanceId): ?array{
         return $this->artistRepository->getAllParticipatingArtistsInPerformance($artistPerformanceId);
     }
-
+    
+    public function getArtistNameByArtistId($id)
+    {
+        return $this->artistRepository->getArtistNameByArtistId($id);
+    }
 }
