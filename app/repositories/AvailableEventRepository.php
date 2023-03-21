acefcfe92be8f29766df0fb4f741faeb6c796398
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-21 07:23:36 +0100

    Update AvailableEventRepository.php

--- app/repositories/AvailableEventRepository.php
+++ app/repositories/AvailableEventRepository.php
@@ -29,3 +29,79 @@ class AvailableEventRepository extends repository
         }
     }
 }
+
+
+
+  private function checkAvailableEventExistence($stmt): bool
+    {
+        try {
+            $stmt->execute();
+            if ($stmt->rowCount() > 0) {
+                return true;
+            }
+            return false;
+        } catch (PDOException $e) {
+            $message = '[' . date("F j, Y, g:i a e O") . ']' . $e->getMessage() . $e->getCode() . $e->getFile() . ' Line ' . $e->getLine() . PHP_EOL;
+            error_log("Database connection failed: " . $message, 3, __DIR__ . "/../Errors/error.log");
+            http_response_code(500);
+            exit();
+        }
+    }
+
+
+    public function  getAvailableEventByIdWithUrl($id)
+    {
+        try {
+            
+            $stmt = $this->connection->prepare("SELECT * From availableevent WHERE eventId LIKE :id");
+            $stmt->bindValue(':id', "%$id%");
+             if ($this->checkAvailableEventExistence($stmt)) {
+                $stmt->execute();
+                $result = $stmt->fetch();
+                return $result;}
+            
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
+
+    public function  retrieveParticipatingArtistsDataWithUrl($id)
+    {
+        try {
+            
+            $stmt = $this->connection->prepare("SELECT * From availableevent WHERE eventId LIKE :id");
+            $stmt->bindValue(':id', "%$id%");
+             if ($this->checkAvailableEventExistence($stmt)) {
+                $stmt->execute();
+                $result = $stmt->fetch();
+                //echo $result[5];
+                if ($result[5] != NULL){
+                    return true;
+                }
+                return false;
+            }
+            
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
+
+
+    public function getEventNameByEventTypeIdWithUrl($id)
+    {
+        try {
+            
+            $stmt = $this->connection->prepare("SELECT eventName From event WHERE eventId LIKE :id");
+            $stmt->bindValue(':id', "%$id%");
+             if ($this->checkAvailableEventExistence($stmt)) {
+                $stmt->execute();
+                $result = $stmt->fetch();
+                return $result[0];
+                
+            }
+            
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
+
