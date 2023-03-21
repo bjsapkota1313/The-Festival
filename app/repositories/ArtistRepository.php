1e95707693e0fb7e46a13d26973e56569472df5a
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-21 07:24:27 +0100

    Update ArtistRepository.php

--- app/repositories/ArtistRepository.php
+++ app/repositories/ArtistRepository.php
@@ -141,4 +141,36 @@ class ArtistRepository extends Repository
             echo $e;
         }
     }
+    
+      private function checkArtistExistence($stmt): bool
+    {
+        try {
+            $stmt->execute();
+            if ($stmt->rowCount() > 0) {
+                return true;
+            }
+            return false;
+        } catch (PDOException $e) {
+            $message = '[' . date("F j, Y, g:i a e O") . ']' . $e->getMessage() . $e->getCode() . $e->getFile() . ' Line ' . $e->getLine() . PHP_EOL;
+            error_log("Database connection failed: " . $message, 3, __DIR__ . "/../Errors/error.log");
+            http_response_code(500);
+            exit();
+        }
+    }
+
+    public function  getArtistByIdWithUrl($id)
+    {
+        try {
+            
+            $stmt = $this->connection->prepare("SELECT * From artist WHERE artistId LIKE :id");
+            $stmt->bindValue(':id', "%$id%");
+             if ($this->checkArtistExistence($stmt)) {
+                $stmt->execute();
+                $result = $stmt->fetch();
+                return $result;}
+            
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
 }
