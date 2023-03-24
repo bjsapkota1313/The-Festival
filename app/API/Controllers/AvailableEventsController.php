13be2c8ed67d0948f3d0d3a38b8bd9a22432288a
MariaRaluca Diaconescu <90312191+MariaRalucaDiaconescu@users.noreply.github.com>
2023-03-25 00:08:51 +0100

    Update AvailableEventsController.php

--- app/API/Controllers/AvailableEventsController.php
+++ app/API/Controllers/AvailableEventsController.php
@@ -84,6 +84,24 @@ class AvailableEventsController extends ApiController
             echo $e->getMessage();
         }
     }
+    
+    
+       public function retrieveArtistData(){
+        try {
+            $this->sendHeaders();
+            $artist=NULL;
+
+            if (!empty($_GET['id'])) {
+                $id = htmlspecialchars($_GET['id']);
+                $artist = $this->availableEventService->getParticipatingArtistByIdWithUrl($id);
+            }
+            echo Json_encode($artist);
+        }
+        catch (InvalidArgumentException|Exception $e) {
+            http_response_code(500); // sending bad request error to APi request if something goes wrong
+            echo $e->getMessage();
+        }
+    }
 }
 
 
